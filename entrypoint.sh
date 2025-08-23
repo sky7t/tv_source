45616242cbda48dacc40f9a39460605523706c70
sky7t <163654567+sky7t@users.noreply.github.com>
2025-08-23 11:20:33 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,1 +1,10 @@
+#!/bin/sh
 echo "HELLO WORLD"
+
+echo ">>> Starting custom entrypoint..."
+# Do any setup you need here
+# e.g., copying config files, env substitution, etc.
+
+# Finally, start nginx in the foreground
+nginx -g 'daemon off;'
+
