5d4f9a97b30ce8fe242a3d7b29f2acd35bfeb773
sky7t <163654567+sky7t@users.noreply.github.com>
2025-09-01 15:52:10 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,14 @@
 echo "HELLO WORLD"
 
 echo ">>> Starting custom entrypoint..."
-# Do any setup you need here
-# e.g., copying config files, env substitution, etc.
 
-# Finally, start nginx in the foreground
-nginx -g 'daemon off;'
+echo ">>> Current directory: $(pwd)"
+
+
+php -S localhost:5002 -t /app/zee5 > server.log 2>&1 & disown
+
+php -S localhost:5003 -t /app/tata1 > server.log 2>&1 & disown
+
+
+
 
