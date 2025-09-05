8754627cb81932bf5bf5dd6cd2dea3962b479092
sky7t <163654567+sky7t@users.noreply.github.com>
2025-09-05 13:15:44 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,16 +5,16 @@ echo "HELLO WORLD"
 echo ">>> Starting custom entrypoint..."
 echo ">>> Current directory: $(pwd)"
 
-# # Start jiotv_go server in background, log output to a file for debug
-# # jiotv_go serve --port 5001 --public > /var/log/jiotv_go.log 2>&1 &
+# Start jiotv_go server in background, log output to a file for debug
+# jiotv_go serve --port 5001 --public > /var/log/jiotv_go.log 2>&1 &
 
-# # Start php web server for zee app
-# cd /app/zee
-# php -S 0.0.0.0:5002 > /var/log/php_zee.log 2>&1 &
+# Start php web server for zee app
+cd /app/zee
+php -S 0.0.0.0:5002 > /var/log/php_zee.log 2>&1 &
 
-# # Start php web server for tata1 app
-# cd /app/tata1
-# php -S 0.0.0.0:5003 > /var/log/php_tata1.log 2>&1 &
+# Start php web server for tata1 app
+cd /app/tata1
+php -S 0.0.0.0:5003 > /var/log/php_tata1.log 2>&1 &
 
-# # Optionally, wait to keep container running
-# # wait
+# Optionally, wait to keep container running
+# wait
