6b98d168969ae679bbb85263482ddc61ea22e09a
sky7t <163654567+sky7t@users.noreply.github.com>
2025-09-05 14:20:32 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -9,11 +9,11 @@ echo ">>> Current directory: $(pwd)"
 ./root/.jiotv_go/bin/jiotv_go serve --port 5101 --public > /var/log/jiotv_go.log 2>&1 &
 
 # Start php web server for zee app
-cd /app/zee
+cd vox/app/zee
 php -S 0.0.0.0:5102 > /var/log/php_zee.log 2>&1 &
 
 # Start php web server for tata1 app
-cd /app/tata1
+cd vox/app/tata1
 php -S 0.0.0.0:5103 > /var/log/php_tata1.log 2>&1 &
 
 wait
