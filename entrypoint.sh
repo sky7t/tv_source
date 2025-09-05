87125e2003c8b7f82d2e2daa157d4ee9f61d014d
sky7t <163654567+sky7t@users.noreply.github.com>
2025-09-05 16:43:30 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "HELLO WORLD"
-echo ">>> Starting custom entrypoint..."
-echo ">>> Current directory: $(pwd)"
-
-# Start jiotv_go if installed
 if [ -f "./vox/jio/jiotvgo" ]; then
   echo "Starting JioTV Go..."
   ./vox/jio/jiotvgo --config /vox/jio/jiotv_go.json serve --port 5001 --public > /var/log/jiotv_go.log 2>&1 &
@@ -15,7 +10,6 @@ else
   chmod +x install.sh
   bash install.sh
   
-    # Copy the binary if it exists
     if [ -f ./root/.jiotv_go/bin/jiotv_go ]; then
         mkdir -p /vox/jio
         cp ./root/.jiotv_go/bin/jiotv_go /vox/jio/jiotvgo
@@ -29,29 +23,12 @@ else
   ./vox/jio/jiotvgo --config /vox/jio/jiotv_go.json serve --port 5001 --public > /var/log/jiotv_go.log 2>&1 &
 fi
 
-# Clone only if folder doesn’t exist (to avoid recloning every run)
 [ ! -d "vox/zee5" ] && git clone https://github.com/yuvraj824/zee5 vox/zee5
 [ ! -d "vox/tata1" ] && git clone https://github.com/drmlive/tataplay vox/tata1
 
 
-# Start your PHP servers or whatever
 echo "Starting Zee5..."
 php -S 0.0.0.0:5002 -t vox/zee5 > /var/log/php_zee.log 2>&1 &
 echo "Starting TataPLAY..."
 php -S 0.0.0.0:5003 -t vox/tata1 > /var/log/php_tata1.log 2>&1 &
 wait
-
-
-
-# # Start jiotv_go server in background, log output to a file for debug
-# ./root/.jiotv_go/bin/jiotv_go serve --port 5101 --public > /var/log/jiotv_go.log 2>&1 &
-
-# # Start php web server for zee app
-# cd vox/app/zee
-# php -S 0.0.0.0:5102 > /var/log/php_zee.log 2>&1 &
-
-# # Start php web server for tata1 app
-# cd vox/app/tata1
-# php -S 0.0.0.0:5103 > /var/log/php_tata1.log 2>&1 &
-
-# wait
